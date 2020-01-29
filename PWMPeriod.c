be221b61557b14df865733f62dc020648ca47087
mstraubmuller <60440956+mstraubmuller@users.noreply.github.com>
2020-01-29 11:51:42 -0600

    Update PWMPeriod.c

--- PWMPeriod.c
+++ PWMPeriod.c
@@ -1,16 +1,14 @@
-int pin_number = 13;
+int pin_number = 11;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin_number, OUTPUT); //same pin as default led; good for ensuring problem is hardware
+  pinMode(pin_number, OUTPUT); //intentionally not the same pin as led; prevents current from being broken
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  test_PWM(750, 500, 10, pin_number);
-  delay(1000);
-  test_PWM(250, 100, 10, pin_number);
-  delay(1000);
+  test_PWM(250, 250, 10, pin_number);
+  delay(250);
 }
 
 void test_PWM(int dur, int pwr, int prd, int pin) {
